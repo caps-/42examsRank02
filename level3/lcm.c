765f3c40f91aac8a3ed2cd15b71260a0b1c4506b
Phil Whittington <pwhittin@F2K028.42adel.org.au>
2023-01-18 15:18:05 +1030

    updated comments for lcm

--- level3/lcm.c
+++ level3/lcm.c
@@ -5,10 +5,11 @@
  * If a is 10 and b is 15 then the LCM would be 30. Get it? Cool.
  *
  * So basically we figure out which number is bigger, a or b, and then give n
- * that value. Then we just jump in to a recursive loop that checks to see if
- * the remainder of n divided by both and b is zero. If it is, it means they're
- * we've found the lowest common multiple! If not, we increment n by one and
- * then repeat the loop. */
+ * that value. Why? Because the lcm must be at least whatever the highest value
+ * is that we passed to the function. Get me? Then we just jump in to a recursive
+ * loop that checks to see if the remainder of n divided by both and b is zero. 
+ * If it is, it means we've found the lowest common multiple! If not, we 
+ * increment n by 1 and then repeat the loop. */
 unsigned int	lcm(unsigned int a, unsigned int b)
 {
 	unsigned int	n;
