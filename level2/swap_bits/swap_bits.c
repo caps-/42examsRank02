336385523a38181f12bb127f3a8e84754208466d
Caps <caps@plus618.com>
2023-05-30 18:18:17 +0930

    added swap_bits.c

---
+++ level2/swap_bits/swap_bits.c
@@ -0,0 +1,42 @@
+/* Right, so let's take the first character we're passing to this function from
+ * the main, 't'. The function takes a char which is 8 bits or one byte and
+ * swaps the two halves of the byte around (first four and last four bits).
+ * In this case 't' becomes 'G':
+ *
+ * 		t = 0111 0100
+ * 		G = 0100 0111
+ *
+ * So here's how we do it. Right shifting by 4:
+ *
+ * 		t = 0111 0100
+ *   >> 4 = 0000 0111
+ *
+ * If we shift left by 4 we get:
+ * 		t = 0111 0100
+ *   << 4 = 0100 0000
+ *
+ * If we use those with an OR operator the following happens:
+ *
+ *		0000 0111
+ *	  | 0100 0000
+ *	    ---------
+ *		0100 0111
+ *
+ * Boom! We've just swapped the bits around, and in this case the result is the
+ * binary value for 'G'! */
+unsigned char	swap_bits(unsigned char octet)
+{
+	return ((octet >> 4 | octet << 4));
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char	c1 = 't';
+	char	c2 = 'G';
+
+	printf("\"t\" after bits have been swapped: %c\n", swap_bits(c1));
+	printf("\"G\" after bits have been swapped: %c\n", swap_bits(c2));
+	return (0);
+}
