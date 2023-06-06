352e7b3e9754bfbe5fc24d5b522be40f39ec2834
Caps <caps@plus618.com>
2023-06-06 17:15:20 +0930

    updated pgcd

--- level3/pgcd/pgcd.c
+++ level3/pgcd/pgcd.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int	pgcd(unsigned int n1, unsigned int n2)
+void	pgcd(unsigned int n1, unsigned int n2)
 {
-	int	i = n1;
-
-	while (i > 0)
+	if (n1 > 0 && n2 > 0)
 	{
-		if (n1 % i == 0 && n2 % i == 0)
-			return (i);
-		i--;
+		while (n1 != n2)
+		{
+			if (n1 > n2)
+			{
+				n1 -= n2;
+			}
+			else
+			{
+				n2 -= n1;
+			}
+		}
+		printf("%d", n1);
 	}
-	return (1);
 }
 
-int	main(int argc, char const **argv)
+int	main(int argc, char **argv)
 {
 	if (argc == 3)
-		printf("%d", pgcd(atoi(argv[1]), atoi(argv[2])));
+		pgcd(atoi(argv[1]), atoi(argv[2]));
 	printf("\n");
 	return (0);
 }
