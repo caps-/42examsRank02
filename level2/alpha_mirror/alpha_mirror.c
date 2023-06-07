6b17a6eea84d86974bc1bedfb9b2d7bb1aaebba1
Caps <caps@plus618.com>
2023-06-07 15:56:01 +0930

    added alpha_mirror

---
+++ level2/alpha_mirror/alpha_mirror.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+void	alpha_mirror(char *s)
+{
+	int	i = 0;
+	while (s[i])
+	{
+		if (s[i] >= 'A' && s[i] <= 'Z')
+		{
+			s[i] = 'Z' - s[i] + 'A';
+		}
+		else if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = 'z' - s[i] + 'a';
+		}
+		write(1, &s[i++], 1);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+		alpha_mirror(argv[1]);
+	write(1, "\n", 1);
+	return (0);
+}
