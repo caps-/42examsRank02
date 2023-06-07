44003ed5c1d8e1001fa8e2513232d094755a6b85
Caps <caps@plus618.com>
2023-06-07 18:13:41 +0930

    Updated add_prime_sum

--- level3/add_prime_sum/add_prime_sum.c
+++ level3/add_prime_sum/add_prime_sum.c
@@ -1,11 +1,23 @@
 #include <unistd.h>
 
+/* Function to write numbers to the output. */
 void	ft_putnbr(int n)
 {
 	char c;
 
+	//If the number passed to the program isn't positive we need to display a 0
+	//on the ouput.
 	if (n < 0)
 		write(1, "0", 1);
+	//If it's less than 10 we write n + 48, which will be the ascii value for
+	//that particular number. If it's larger than 10 we write the number divided
+	//by 10, then the modulo/remainder. So like if it's the number 23, the else statement
+	//would be saying:
+	//
+	//ft_putnbr(20 / 10 = 2);
+	//ft_putnbr(20 % 10 = 3);
+	//
+	//Which would write 23 to the output. Cool!
 	if (n < 10)
 	{
 		c = n + 48;
@@ -18,6 +30,7 @@ void	ft_putnbr(int n)
 	}
 }
 
+/* An implementation of the atoi function. */
 int	ft_atoi(char *str)
 {
 	int result = 0;
@@ -37,29 +50,31 @@ int	ft_atoi(char *str)
 	return (sign * result);
 }
 
+/* Much nicer (imo) function to check if a number is prime using the square root
+ * method. */
 int	is_prime(int n)
 {
-	int	i = 3;
-
+	//Is the number 1? Or even worse, negative? Sorry, not a prime!
 	if (n <= 1)
 		return (0);
-	if (n % 2 == 0 && n > 2)
-		return (0);
-	while (i < (n / 2))
+	//I won't go in to all of the maths here, but basically we only need to
+	//divide by numbers up to the square root of n to see if n is prime or not.
+	//If n is divisible by any of those it means it's not a prime number.
+	for (int i = 2; i * i <= n; i++)
 	{
 		if (n % i == 0)
 			return (0);
-		i += 2;
 	}
 	return (1);
 }
 
+/* Simple main, we just check to see if any of the numbers <= the one passed to
+ * the program are primes, and if they are we sum them all up.*/
 int	main(int argc, char **argv)
 {
-	int	sum = 0;
-
 	if (argc == 2)
 	{
+		int	sum = 0;
 		int	number = ft_atoi((argv[1]));
 
 		while (number > 0)
