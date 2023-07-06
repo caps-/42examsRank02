15470d323c29896dfbed5110f78ad14fb62eac42
Caps <caps@plus618.com>
2023-07-06 10:25:50 +0930

    rostring update, will complete later lol

--- level4/rostring/rostring.c
+++ level4/rostring/rostring.c
@@ -49,3 +49,61 @@ int	main(int argc, char **argv)
 	write(1, "\n", 1);
 	return (0);
 }
+
+/* 
+#include <unistd.h>
+
+int	is_whitespace(char c)
+{
+	return (c == ' ' || c == '\t');
+}
+
+/* OK, here's how this works. It begins by moving past any whitespace at the
+ * start and then moving through the first word, as well as marking the start
+ * and end positions of it. Once that's done it can move on to the rest of the
+ * string, working inside a while loop. Remember that each word must only be
+ * seperated by ONE space, so that while condition on line xx actually checks to
+ * see if there's more than one and moves through them, leaving only one space
+ * before the next word. Next it prints */
+ /*
+void	rostring(char *str)
+{
+	int	i = 0;
+	int word_start = 0;
+	int	word_end = 0;
+	int	flag = 0; //will mark where the first word needs to be moved to
+
+	//Dealing with the first word.
+	while (is_whitespace(str[i]))
+		i++;
+	word_start = i;
+	while (str[i] && !is_whitespace(str[i]))
+		i++;
+	word_end = i;
+	while (str[i] && is_whitespace(str[i]))
+		i++;
+	//Now for the rest of the stirng. Moves past whitespace, writes te
+	while (str[i])
+	{
+		while (is_whitespace(str[i]) && is_whitespace(str[i + 1]))
+			i++;
+		if (is_whitespace(str[i]))
+			flag = 1;
+		write(1, &str[i], 1);
+		i++;
+	}
+	//Deal with the last word.
+	if (flag)
+		write(1, " ", 1);
+	while (word_start < word_end)
+		write(1, &str[word_start++], 1);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+		rostring(argv[1]);
+	write(1, "\n", 1);
+	return (0);
+}
+*/
