7b5df1a5ace9aeac747015f97fd1e36511da4aba
Caps <caps@plus618.com>
2023-07-07 20:31:03 +0930

    refatored ft_atoi

--- level2/ft_atoi/ft_atoi.c
+++ level2/ft_atoi/ft_atoi.c
@@ -13,21 +13,33 @@
  * 50. So what we're doing this time round is actually adding 50 + 7 and
  * sticking THAT in our 'number variable, which if we return it gives the
  * integer value of 57! */
-int	ft_atoi(const char *str)
-{
-	int	number = 0;
-	int	sign = 1;
 
-	while (*str == ' ' || (*str >= 9 && *str <= 13))
-		str++;
-	if (*str == '-')
-		sign = -1;
-	if (*str == '-' || *str == '+')
-		str++;
-	while (*str >= '0' && *str <= '9')
-	{
-		number = number * 10 + *str - 48;
-		str++;
-	}
-	return (number * sign);
+int     ft_atoi(const char *str)
+{
+        int     sign = 1;
+        int     result = 0;
+
+        while (*str <= 32)
+                str++;
+        if (*str == '-')
+                sign = -1;
+        if (*str == '-' || *str == '+')
+                str++;
+        while (*str >= '0' && *str <= '9')
+        {
+                result = result * 10 + *str - 48;
+                str++;
+        }
+        return (sign * result);
 }
+
+/*
+#include <stdio.h>
+
+int     main(void)
+{
+        printf("%d\n", ft_atoi("42"));
+        printf("%d\n", ft_atoi("-4242"));
+        return (0);
+}
+*/
