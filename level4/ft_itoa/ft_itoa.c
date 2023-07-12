dd38f4ca1445b0749711b450e8a96cefcd67ef6d
Caps <caps@plus618.com>
2023-07-12 14:26:53 +0930

    ft_itoa comments added

--- level4/ft_itoa/ft_itoa.c
+++ level4/ft_itoa/ft_itoa.c
@@ -1,3 +1,8 @@
+/* Converts an integer into a string. Actually not a lot to this, we just get
+ * the length the string needs to be and allocate mem for it and then fill it up
+ * accordingly. The actual conversion from a single digit int to a char is super
+ * easy, it's literally just the int + 48. */
+
 #include <stdlib.h>
 
 char	*ft_itoa(int nbr)
@@ -8,33 +13,33 @@ char	*ft_itoa(int nbr)
 
 	if (nbr <= 0)
 	{
-		len++;
+		len++; //makes room for a - sign
 	}
 	while (n)
 	{
 		n /= 10;
-		len++;
+		len++; //gets us the length/number of digits
 	}
-	result = (char *)malloc(sizeof(char *) * (len + 1));
+	result = malloc(sizeof(char *) * (len + 1));
 	if (result == NULL)
 		return (NULL);
-	result[len] = '\0';
+	result[len] = '\0'; //adds the null terminator on the end of the string
 	if (nbr == 0)
 	{
-		result[0] = '0';
+		result[0] = '0'; //gotta do this here, won't work in the main while loop below
 		return (result);
 	}
 	if (nbr < 0)
 	{
-		result[0] = '-';
+		result[0] = '-'; //stick the - sign at the start of the string
 		nbr *= -1;
 	}
 	while (nbr)
 	{
-		result[--len] = nbr % 10 + 48;
-		nbr /= 10;
+		result[--len] = nbr % 10 + 48; //put the char in the string
+		nbr /= 10; //go to the next digit
 	}
-	return (result);
+	return (result); //boom ting!
 }
 
 /*
@@ -44,9 +49,10 @@ int	main(void)
 {
 	char	*s1 = ft_itoa(9823);
 	char	*s2 = ft_itoa(-321);
+	char	*s3 = ft_itoa(0);
 
-	printf("----------\n");
-	printf("%s, %s\n", s1, s2);
+	printf("------------\n");
+	printf("%s, %s, %s\n", s1, s2, s3);
 	return (0);
 }
 */
