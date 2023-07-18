dc54bb19efd90efb807e7762f7e971048055b50f
Caps <caps@plus618.com>
2023-07-18 19:09:39 +0930

    ft_strdup refactor

--- level2/ft_strdup/ft_strdup.c
+++ level2/ft_strdup/ft_strdup.c
@@ -1,17 +1,18 @@
-#include <stdlib.h>
-
 /* Pretty simple. Get the length of the string so we can allocate the right
  * amount of memory to copy the string in to. Copy the shits over, stick a null
  * terminator on the end and return the duplicated string.*/
+
+#include <stdlib.h>
+
 char	*ft_strdup(char *src)
 {
 	int		i = 0;
 	int		length = 0;
 	char	*duplicate;
 
-	while (src[length])
-		length++;
-	duplicate = malloc(sizeof(*duplicate) * (length + 1));
+	while (src[length++]);
+	duplicate = malloc(sizeof(char) * (length + 1));
+
 	if (duplicate != NULL)
 	{
 		while (src[i])
@@ -32,15 +33,12 @@ int	main(void)
 {
 	char	*s1 = "test";
 	char	*s2 = strdup(s1);
-	char	*s3 = "test";
-	char	*s4 = ft_strdup(s1);
+	char	*s3 = ft_strdup(s1);
 
-	printf("[strdup]-------------\n");
-	printf("%s\n", s1);
-	printf("%s\n", s2);
-	printf("[ft_strdup]-----------\n");
-	printf("%s\n", s3);
-	printf("%s\n", s4);
+	printf("---------------\n");
+	printf("Original : %s\n", s1);
+	printf("strdup   : %s\n", s2);
+	printf("ft_strdup: %s\n", s3);
 	return (0);
 }
 */
