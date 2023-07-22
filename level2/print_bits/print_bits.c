39cc2e5602d3ae6c000fe2cb98f9a9ef15e01712
caps <caps@plus618.com>
2023-07-22 16:14:49 +0930

    print_bits commented

--- level2/print_bits/print_bits.c
+++ level2/print_bits/print_bits.c
@@ -1,3 +1,21 @@
+/* Some bitwise stuff fi dem. For each bit we right shift octet and then do a
+ * bitwise AND operation on it with whatever value i currentl has. Quick,
+ * handwavy reminder of how the & operator works:
+ *
+ * It takes two numbers, compares the bits of each one and then returns 1 if the
+ * corresponding bits are both 1. So for example:
+ *
+ *   0101 (int 5)
+ * & 0011 (int 3)
+ *   ----
+ * = 0001 (int 1)
+ *
+ *  So an unsigned char has 8 bits, so we need to do that 8 times with
+ *  decerementing values of i, getting 1 or 0 as a resulit. This will actually
+ *  end up being the order of bits that we need to make the binary 
+ *  representation of the number we've given to the function! Adding 48 just
+ *  gives us the right ascii value to print. */
+
 #include <unistd.h>
 
 void	print_bits(unsigned char octet)
